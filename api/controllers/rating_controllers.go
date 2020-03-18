f5ea04d94e0ac6fe370b989322e5482c2097ac0c
nayon <nayon@genex.digital>
2020-03-18 11:16:58 +0600

    Rating model and controller structure add

--- api/controllers/rating_controllers.go
+++ api/controllers/rating_controllers.go
@@ -1,23 +1,21 @@
 package controllers
 
-import "net/http"
-
-func (server *Server) CreateRating(w http.ResponseWriter, r *http.Request){
+func (server *Server) CreateRating(){
 
 }
 
-func (server *Server) GetRatings(w http.ResponseWriter, r *http.Request){
+func (server *Server) GetRatings(){
 
 }
 
-func (server *Server) GetRating(w http.ResponseWriter, r *http.Request) {
+func (server *Server) GetRating(){
 
 }
 
-func (server *Server) UpdateRating(w http.ResponseWriter, r *http.Request){
+func (server *Server) UpdateRating(){
 
 }
 
-func (server *Server) DeleteRating(w http.ResponseWriter, r *http.Request){
-
+func (server *Server) DeleteRating(){
+	
 }
