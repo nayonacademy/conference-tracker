5e18b31a6383d0dbed63f2c446644733fb3b6564
Shariful Islam <si.nayon@gmail.com>
2020-04-26 12:57:46 +0600

    build

--- api/auth/token.go
+++ api/auth/token.go
@@ -27,7 +27,7 @@ func TokenValid(r *http.Request) error {
 	tokenString := ExtractToken(r)
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(os.Getenv("API_SECRET")), nil
 	})
