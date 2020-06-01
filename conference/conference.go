5c11692da0216f458f7e0b6818d5124b551983fc
nayon <nayon@genex.digital>
2020-06-01 10:52:23 +0600

    conference interface

--- conference/conference.go
+++ conference/conference.go
@@ -1,8 +1,10 @@
 package conference
 
 import (
+	"errors"
 	"github.com/jinzhu/gorm"
 	"time"
+	"context"
 )
 
 type ConfOwnProfile struct {
@@ -74,27 +76,43 @@ type Speaker struct {
 }
 
 type Repository interface {
-	CreateConfOwnProfile()
-	UpdateConfOwnProfile()
-	DeleteConfOwnProfile()
+	CreateConfOwnProfile(ctx context.Context, confprofile ConfOwnProfile) error
+	GetConfOwnProfile(ctx context.Context, id string)(interface{}, error)
+	UpdateConfOwnProfile(ctx context.Context, confprofile ConfOwnProfile)(interface{}, error)
+	DeleteConfOwnProfile(ctx context.Context, id string)(string, error)
 
-	CreateConference()
-	UpdateCreateConference()
-	DeleteCreateConference()
+	CreateConference(ctx context.Context, conference Conference) error
+	GetConference(ctx context.Context, id string)(interface{}, error)
+	UpdateCreateConference(ctx context.Context, confprofile ConfOwnProfile)(interface{}, error)
+	DeleteCreateConference(ctx context.Context, id string)(string, error)
 
-	CreateLocation()
-	UpdateCreateLocation()
-	DeleteCreateLocation()
+	CreateLocation(ctx context.Context, location Location) error
+	GetLocation(ctx context.Context, id string)(interface{}, error)
+	UpdateCreateLocation(ctx context.Context, confprofile ConfOwnProfile)(interface{}, error)
+	DeleteCreateLocation(ctx context.Context, id string)(string, error)
 
-	CreateRating()
-	UpdateCreateRating()
-	DeleteCreateRating()
+	CreateRating(ctx context.Context, rating Rating) error
+	GetRating(ctx context.Context, id string)(interface{}, error)
+	UpdateCreateRating(ctx context.Context, confprofile ConfOwnProfile)(interface{}, error)
+	DeleteCreateRating(ctx context.Context, id string)(string, error)
 
-	CreateReport()
-	UpdateCreateReport()
-	DeleteCreateReport()
+	CreateReport(ctx context.Context, report Report) error
+	GetReport(ctx context.Context, id string)(interface{}, error)
+	UpdateCreateReport(ctx context.Context, confprofile ConfOwnProfile)(interface{}, error)
+	DeleteCreateReport(ctx context.Context, id string)(string, error)
 
-	CreateSpeaker()
-	UpdateCreateSpeaker()
-	DeleteCreateSpeaker()
+	CreateSpeaker(ctx context.Context, speaker Speaker) error
+	GetSpeaker(ctx context.Context, id string)(interface{}, error)
+	UpdateCreateSpeaker(ctx context.Context, confprofile ConfOwnProfile)(interface{}, error)
+	DeleteCreateSpeaker(ctx context.Context, id string)(string, error)
+}
+
+func GetAllUsers () []*Conference{
+	accs := []*Conference{}
+	err := errors.New("write query here")
+	if err != nil{
+		panic("error")
+
+	}
+	return accs
 }
